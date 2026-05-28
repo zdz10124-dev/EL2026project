9d0f52cfeff01bedb10657e390b34cf0dadfb549
FaultCutter <522025330072@smail.nju.edu.cn>
2026-05-28 17:41:38 +0800

    增加windows数据库支持更新readme

--- today_eat_app/lib/services/database_service.dart
+++ today_eat_app/lib/services/database_service.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import '../models/meal_record.dart';
 
